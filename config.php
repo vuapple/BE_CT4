b6c87fd7d4b2c4df3a0438f36e781459212b659b
vuapple <ngoanhmtu2002@gmail.com>
2021-11-03 16:55:47 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+/** The name of the database for WordPress */
+define( 'DB_NAME', 'nhom4' );
+/** MySQL database username */
+define( 'DB_USER', 'root' );
+/** MySQL database password */
+define( 'DB_PASSWORD', '' );//Nếu có mật khẩu thì nhập " root " còn ko có thì bỏ trống
+/** MySQL hostname */
+define( 'DB_HOST', 'localhost' );
+/** port number of DB */
+define( 'PORT', 3306);//mariaDB:3309
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
---
+++ models/product.php
@@ -0,0 +1,32 @@
+<?php
+class Product extends Db
+{
+    public function getAllProducts()
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products");
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function getProductById($id)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products WHERE id = ?");
+        $sql->bind_param("i", $id);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+    public function search($keyword)
+    {
+        $sql = self::$connection->prepare("SELECT * FROM products 
+        WHERE `name` LIKE ?");
+        $keyword = "%$keyword%";
+        $sql->bind_param("s", $keyword);
+        $sql->execute(); //return an object
+        $items = array();
+        $items = $sql->get_result()->fetch_all(MYSQLI_ASSOC);
+        return $items; //return an array
+    }
+}
